c1f3e15490db35fd429da0bdbe10ab9b67c928e5
Ronnie Princi <rprinci@gmail.com>
2021-08-13 13:00:45 +0200

    Node + CRUD

--- src/index.js
+++ src/index.js
@@ -2,8 +2,39 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' });
+app.get('/products', (request, response) => {
+  return response.json([
+    "Product 1",
+    "Product 2",
+    "Product 3",
+    "Product 4"
+  ]);
+});
+
+app.post('/products', (request, response) => {
+  return response.json([
+    "Product 1",
+    "Product 2",
+    "Product 3",
+    "Product 4"
+  ]);
+});
+
+app.put('/products/:id', (request, response) => {
+  return response.json([
+    "Product 10",
+    "Product 2",
+    "Product 3",
+    "Product 4"
+  ]);
+});
+
+app.delete('/products/:id', (request, response) => {
+  return response.json([
+    "Product 2",
+    "Product 3",
+    "Product 4"
+  ]);
 });
 
 app.listen(3333, () => {
